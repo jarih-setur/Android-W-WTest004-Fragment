da98bc5cb7a842985db0afdff60c315c3134e2c3
Jari í Hjøllum <jarih@setur.fo>
2020-02-24 15:40:52 +0000

    1st

---
+++ app/src/main/java/com/example/wtest004_fragment/MainActivity.java
@@ -0,0 +1,23 @@
+kkkpackage com.example.wtest004_fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.icu.lang.UProperty;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.wtest004_fragment.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG,"onCreate: started");
+
+        )
+    }
+}
---
+++ app/src/main/java/com/example/wtest004_fragment/SectionsStatePagerAdapter.java
@@ -0,0 +1,25 @@
+package com.example.wtest004_fragment;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class SectionsStatePagerAdapter extends FragmentStatePagerAdapter {
+
+    private final List<Fragment> mFragmentList = new ArrayList();
+    private final List<Fragment> mFragmentTitleList = new ArrayList();
+
+    public SectionsStatePagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
