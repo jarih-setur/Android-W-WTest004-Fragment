d5832d44dc255f35e4dd75543bbfc580c9b2dacf
Jari í Hjøllum <jarih@setur.fo>
2020-02-25 15:53:42 +0000

    end of 25-02-2020 JH

--- app/src/main/java/com/example/wtest004_fragment/MainActivity.java
+++ app/src/main/java/com/example/wtest004_fragment/MainActivity.java
@@ -1,6 +1,7 @@
-kkkpackage com.example.wtest004_fragment;
+package com.example.wtest004_fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.icu.lang.UProperty;
 import android.os.Bundle;
@@ -11,6 +12,8 @@ import com.example.wtest004_fragment.R;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private SectionsStatePagerAdapter mSectionsStatePagerAdapter;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG,"onCreate: started");
 
-        )
+        mSectionsStatePagerAdapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+
+        mViewPager = (ViewPager) findViewById(R.id.container);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        SectionsStatePagerAdapter adapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Fragment1(),"Fragment1");
+        adapter.addFragment(new Fragment1(),"Fragment2");
+        adapter.addFragment(new Fragment1(),"Fragment3");
+    }
+
+    public void setViewPager(int iFragmentNumber) {
+        mViewPager.setCurrentItem(iFragmentNumber);
     }
 }
--- app/src/main/java/com/example/wtest004_fragment/SectionsStatePagerAdapter.java
+++ app/src/main/java/com/example/wtest004_fragment/SectionsStatePagerAdapter.java
@@ -4,22 +4,30 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SectionsStatePagerAdapter extends FragmentStatePagerAdapter {
 
     private final List<Fragment> mFragmentList = new ArrayList();
-    private final List<Fragment> mFragmentTitleList = new ArrayList(); //test
+    private final List<String> mFragmentTitleList = new ArrayList(); //test
 
     public SectionsStatePagerAdapter(FragmentManager fm) {
         super(fm);
     }
 
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
     @Override
     public Fragment getItem(int position) {
-        return null;
+        return mFragmentList.get(position);
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return mFragmentList.size();
     }
 }
